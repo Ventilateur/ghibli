b5a48722c2e451f0cffd7c4b1ddd4d071ecfcf89
Vu-Hoang Phan <vuhoangphan1307@gmail.com>
2020-08-17 19:48:26 +0200

    add logs

--- api/app.py
+++ api/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask import jsonify
-from db import DB
+from controller import Controller
 import config
 
 app = Flask(__name__)
-db = DB(
+controller = Controller(
     config.REDIS['host'],
     config.REDIS['port'],
     config.REDIS['timeout'],
@@ -14,16 +14,22 @@ db = DB(
 
 @app.route('/movies', methods=['GET'])
 def get_movies():
-    films = db.get_all()
-    return jsonify(films), 200
+    films, code = controller.get_all()
+    return jsonify(films), code
 
 
 @app.route('/movies/<uid>', methods=['GET'])
 def get_movie(uid):
-    film = db.get_film(str(uid))
-    return jsonify(film), 200
+    film, code = controller.get_film(str(uid))
+    return jsonify(film), code
 
 
 @app.route('/_/readiness', methods=['GET'])
 def is_ready():
-    return db.ping(), 200
+    ready, code = controller.ping()
+    return jsonify(ready), code
+
+
+@app.route('/_/liveness', methods=['GET'])
+def is_alive():
+    return '', 200
--- api/controller.py
+++ api/controller.py
@@ -1,28 +1,40 @@
 import redis
 import pickle
 import logging
+from functools import wraps
 
 logger = logging.getLogger(__name__)
 
 
-class DB:
+def log_err(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs), 200
+        except redis.TimeoutError or redis.ConnectionError as e:
+            logger.error(f'Cannot connect to redis: {e}')
+            return None, 500
+    return wrapper
+
+
+class Controller:
 
     def __init__(self, host, port, timeout, key_all_films):
         self.redis = redis.Redis(host=host, port=port, socket_timeout=timeout)
         self.key_all_films = key_all_films
         logger.info(f'Redis configured: {host}:{port}')
-        try:
-            self.redis.ping()
-        except TimeoutError or ConnectionError:
-            logger.error('Cannot connect to redis')
+        self.ping()
 
+    @log_err
     def get_all(self):
         films = self.redis.get(self.key_all_films)
         return pickle.loads(films) if films is not None else None
 
+    @log_err
     def get_film(self, film_id):
         film = self.redis.get(film_id)
         return pickle.loads(film) if film is not None else None
 
+    @log_err
     def ping(self):
         return self.redis.ping()
